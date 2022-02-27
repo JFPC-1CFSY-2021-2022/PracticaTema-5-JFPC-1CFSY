29c2806bb50e941fa2f67cc062c655cecd86321c
Jose Fco Pérez Castillo <jfperezcastillo@gmail.com>
2022-02-27 17:08:35 +0100

    Versión inicial del proyecto JFPC_1CFSY_2021_2022

---
+++ Practica_Tema5_Git_JFPC_1CFSY_2022/Practica_Tema5_Git_JFPC_1CFSY_2022/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_Tema5_Git_JFPC_1CFSY_2022
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
